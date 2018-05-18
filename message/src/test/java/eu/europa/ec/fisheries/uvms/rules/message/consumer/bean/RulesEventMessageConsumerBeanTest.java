28bca3d2452e7a975e68e3369a85be33d86c0795
Peter Eriksson <peter.eriksson@havochvatten.se>
2018-05-18 13:51:38 +0200

    Added sanity rules trigger tests

--- message/src/test/java/eu/europa/ec/fisheries/uvms/rules/message/consumer/bean/RulesEventMessageConsumerBeanTest.java
+++ message/src/test/java/eu/europa/ec/fisheries/uvms/rules/message/consumer/bean/RulesEventMessageConsumerBeanTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 import java.util.ArrayList;
+import java.util.Date;
 import javax.jms.Message;
 import javax.jms.TextMessage;
 import org.jboss.arquillian.container.test.api.RunAsClient;
@@ -12,6 +13,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import eu.europa.ec.fisheries.schema.exchange.module.v1.ProcessedMovementResponse;
 import eu.europa.ec.fisheries.schema.exchange.movement.v1.MovementBaseType;
+import eu.europa.ec.fisheries.schema.exchange.movement.v1.MovementRefTypeType;
 import eu.europa.ec.fisheries.schema.rules.exchange.v1.PluginType;
 import eu.europa.ec.fisheries.schema.rules.module.v1.PingRequest;
 import eu.europa.ec.fisheries.schema.rules.module.v1.PingResponse;
@@ -52,8 +54,9 @@ public class RulesEventMessageConsumerBeanTest extends AbstractMessageTest {
         Message responseMessage = jmsHelper.listenForResponseOnQueue(correlationId, "UVMSExchangeEvent");
         
         ProcessedMovementResponse movementResponse = JAXBMarshaller.unmarshallTextMessage((TextMessage) responseMessage, ProcessedMovementResponse.class);
-        MovementBaseType returnedMovement = movementResponse.getOrgRequest().getMovement();
+        assertThat(movementResponse.getMovementRefType().getType(), is(MovementRefTypeType.MOVEMENT));
         
+        MovementBaseType returnedMovement = movementResponse.getOrgRequest().getMovement();        
         assertThat(returnedMovement.getAssetId().getAssetIdList().get(0).getValue(), is(movement.getAssetId().getAssetIdList().get(0).getValue()));
     }
     
@@ -79,6 +82,32 @@ public class RulesEventMessageConsumerBeanTest extends AbstractMessageTest {
         MovementBaseType returnedMovement2 = movementResponse2.getOrgRequest().getMovement();
         
         assertThat(returnedMovement2.getAssetId().getAssetIdList().get(0).getValue(), is(movement2.getAssetId().getAssetIdList().get(0).getValue()));
+    }
+    
+    @Test
+    @RunAsClient
+    public void setMovementReportNullLatitudeShouldTriggerSanityRuleTest() throws Exception {
+        RawMovementType movement = TestHelper.createBasicMovement();
+        movement.getPosition().setLatitude(null);
+        String request = RulesModuleRequestMapper.createSetMovementReportRequest(PluginType.NAF, movement, "testUser");
+        String correlationId = jmsHelper.sendMessageToRules(request);
+        Message responseMessage = jmsHelper.listenForResponseOnQueue(correlationId, "UVMSExchangeEvent");
+        
+        ProcessedMovementResponse movementResponse = JAXBMarshaller.unmarshallTextMessage((TextMessage) responseMessage, ProcessedMovementResponse.class);
+        assertThat(movementResponse.getMovementRefType().getType(), is(MovementRefTypeType.ALARM));
+    }
+    
+    @Test
+    @RunAsClient
+    public void setMovementReportFutureDateShouldTriggerSanityRuleTest() throws Exception {
+        RawMovementType movement = TestHelper.createBasicMovement();
+        movement.setPositionTime(new Date(new Date().getTime() + 100000));
+        String request = RulesModuleRequestMapper.createSetMovementReportRequest(PluginType.NAF, movement, "testUser");
+        String correlationId = jmsHelper.sendMessageToRules(request);
+        Message responseMessage = jmsHelper.listenForResponseOnQueue(correlationId, "UVMSExchangeEvent");
+        
+        ProcessedMovementResponse movementResponse = JAXBMarshaller.unmarshallTextMessage((TextMessage) responseMessage, ProcessedMovementResponse.class);
+        assertThat(movementResponse.getMovementRefType().getType(), is(MovementRefTypeType.ALARM));
     }
     
     /*
