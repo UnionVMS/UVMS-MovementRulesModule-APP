b5cf6012c7d082046bb5374c9e7697c49316a80a
jemConsid <jonas.mattsson@consid.se>
2018-06-05 15:59:52 +0200

    Fixed some test in AlarmRestResourceTest.java, and found and squished a bug while doing so.

--- rest/src/test/java/eu/europa/ec/fisheries/uvms/movementrules/rest/service/AlarmRestResourceTest.java
+++ rest/src/test/java/eu/europa/ec/fisheries/uvms/movementrules/rest/service/AlarmRestResourceTest.java
@@ -2,10 +2,21 @@ package eu.europa.ec.fisheries.uvms.movementrules.rest.service;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.UUID;
+import javax.inject.Inject;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
+
+import eu.europa.ec.fisheries.schema.movementrules.alarm.v1.AlarmReportType;
+import eu.europa.ec.fisheries.schema.movementrules.alarm.v1.AlarmStatusType;
+import eu.europa.ec.fisheries.uvms.movementrules.service.dao.RulesDao;
+import eu.europa.ec.fisheries.uvms.movementrules.service.entity.AlarmReport;
+import eu.europa.ec.fisheries.uvms.movementrules.service.mapper.AlarmMapper;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.junit.Test;
@@ -19,6 +30,7 @@ import eu.europa.ec.fisheries.schema.movementrules.search.v1.AlarmQuery;
 import eu.europa.ec.fisheries.schema.movementrules.search.v1.AlarmSearchKey;
 import eu.europa.ec.fisheries.schema.movementrules.search.v1.ListPagination;
 import eu.europa.ec.fisheries.uvms.movementrules.rest.service.arquillian.BuildRulesRestDeployment;
+
 
 //@RunAsClient
 @RunWith(Arquillian.class)
@@ -41,32 +53,48 @@ public class AlarmRestResourceTest extends BuildRulesRestDeployment {
         assertThat(alarmList.getAlarms().size(), is(0));
     }
 
-/*
+    @Inject
+    RulesDao rulesDao;
+
     @Test
-    @RunAsClient
     public void updateAlarmStatusTest() throws Exception {
+        AlarmReport alarmReport = getBasicAlarmReport();
+        AlarmReport createdAlarmReport = rulesDao.createAlarmReport(alarmReport);
+
+
+        AlarmReportType input = AlarmMapper.toAlarmReportType(createdAlarmReport);
+        input.setStatus(AlarmStatusType.REJECTED);
+
         String response = getWebTarget()
                 .path("alarms")
                 .request(MediaType.APPLICATION_JSON)
-                .put(Entity.json(new AlarmReportType()), String.class);
-        
+                .put(Entity.json(input), String.class);
+
         assertThat(response, is(notNullValue()));
+        AlarmReportType output = deserializeResponseDto(response, AlarmReportType.class);
+        assertEquals(input.getGuid(), output.getGuid());
+        assertEquals(AlarmStatusType.REJECTED, output.getStatus());
+
     }
-*/
-    
-    /*
+
+
+
+
+
     @Test
-    @RunAsClient
     public void getAlarmReportByGuidTest() throws Exception {
+        AlarmReport alarmReport = getBasicAlarmReport();
+        AlarmReport createdAlarmReport = rulesDao.createAlarmReport(alarmReport);
+
         String response = getWebTarget()
-                .path("alarms/" + "NULL_GUID")
+                .path("alarms/" + createdAlarmReport.getGuid())
                 .request(MediaType.APPLICATION_JSON)
                 .get(String.class);
         
-        String responseString = deserializeResponseDto(response, String.class);
-        assertThat(responseString, is(notNullValue()));
+        AlarmReportType responseAlarmReportType = deserializeResponseDto(response, AlarmReportType.class);
+        assertNotNull(responseAlarmReportType);
+        assertEquals(createdAlarmReport.getGuid(), responseAlarmReportType.getGuid());
     }
-    */
     
     
     @Test
@@ -101,7 +129,16 @@ public class AlarmRestResourceTest extends BuildRulesRestDeployment {
         query.setPagination(pagination);
         return query;
     }
-    
+
+    private AlarmReport getBasicAlarmReport() {
+        AlarmReport alarmReport = new AlarmReport();
+        alarmReport.setAssetGuid(UUID.randomUUID().toString());
+        alarmReport.setStatus(AlarmStatusType.OPEN.value());
+        alarmReport.setUpdated(new Date());
+        alarmReport.setUpdatedBy("Test user");
+        return alarmReport;
+    }
+
     private static <T> T deserializeResponseDto(String responseDto, Class<T> clazz) throws Exception {
         ObjectMapper objectMapper = new ObjectMapper();
         ObjectNode node = objectMapper.readValue(responseDto, ObjectNode.class);
