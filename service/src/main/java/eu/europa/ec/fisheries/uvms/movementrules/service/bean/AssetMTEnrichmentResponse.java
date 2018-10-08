d7ed61615f8f3011c84562325ae01e0fb941e279
thomasfanto <thomas.fanto@consid.se>
2018-10-08 16:46:17 +0200

    added support for assetgroups in data carrier

--- service/src/main/java/eu/europa/ec/fisheries/uvms/movementrules/service/bean/AssetMTEnrichmentResponse.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/movementrules/service/bean/AssetMTEnrichmentResponse.java
@@ -4,16 +4,16 @@ import eu.europa.ec.fisheries.schema.mobileterminal.types.v1.MobileTerminalType;
 import eu.europa.ec.fisheries.uvms.movementrules.service.entity.Asset;
 
 import java.io.Serializable;
-import java.util.Objects;
+import java.util.List;
+import java.util.UUID;
 
 public class AssetMTEnrichmentResponse implements Serializable {
 
-    public AssetMTEnrichmentResponse(){
-
-    }
-
     private MobileTerminalType mobileTerminalType;
     private Asset asset;
+    private List<UUID> assetGroupList = null;
+
+    public AssetMTEnrichmentResponse(){}
 
     public MobileTerminalType getMobileTerminalType() {
         return mobileTerminalType;
@@ -31,17 +31,12 @@ public class AssetMTEnrichmentResponse implements Serializable {
         this.asset = asset;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        AssetMTEnrichmentResponse that = (AssetMTEnrichmentResponse) o;
-        return Objects.equals(mobileTerminalType, that.mobileTerminalType) &&
-                Objects.equals(asset, that.asset);
+    public List<UUID> getAssetGroupList() {
+        return assetGroupList;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(mobileTerminalType, asset);
+    public void setAssetGroupList(List<UUID> assetGroupList) {
+        this.assetGroupList = assetGroupList;
     }
+
 }
