2adfc9735aafe0962792203c8c2f3c4f0020242f
peerik <peter.eriksson@havochvatten.se>
2018-11-26 10:48:42 +0100

    jenkins compatible?

--- service/src/main/java/eu/europa/ec/fisheries/uvms/movementrules/service/message/producer/bean/ExchangeProducerBean.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/movementrules/service/message/producer/bean/ExchangeProducerBean.java
@@ -3,15 +3,19 @@ package eu.europa.ec.fisheries.uvms.movementrules.service.message.producer.bean;
 import eu.europa.ec.fisheries.uvms.commons.message.api.MessageConstants;
 import eu.europa.ec.fisheries.uvms.commons.message.api.MessageException;
 import eu.europa.ec.fisheries.uvms.commons.message.impl.AbstractProducer;
-
+import javax.annotation.Resource;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.jms.Destination;
+import javax.jms.Queue;
 
 @Stateless
 public class ExchangeProducerBean extends AbstractProducer {
 
+    @Resource(name = "java:/jms/queue/UVMSExchangeEvent")
+    private Queue exchangeQueue;
+    
     @Override
     public String getDestinationName() {
         return MessageConstants.QUEUE_EXCHANGE_EVENT;
@@ -19,12 +23,12 @@ public class ExchangeProducerBean extends AbstractProducer {
 
     @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
     public String sendModuleMessage(String text, String function) throws MessageException {
-        return this.sendMessageToSpecificQueueWithFunction(text, getDestination(), null, function, null);
+        return this.sendMessageToSpecificQueueWithFunction(text, exchangeQueue, null, function, null);
     }
 
     @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
     public String sendModuleMessage(String text, Destination replyTo, String function) throws MessageException {
-        return this.sendMessageToSpecificQueueWithFunction(text, getDestination(), replyTo, function, "");
+        return this.sendMessageToSpecificQueueWithFunction(text, exchangeQueue, replyTo, function, "");
     }
 
 }
