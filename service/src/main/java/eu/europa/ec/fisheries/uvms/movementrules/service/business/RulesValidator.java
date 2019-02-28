0eee7f927708b6bca02ad9ee5036bfe1baed9782
Jonas E.Mattsson <jonas.mattsson@consid.se>
2019-02-28 13:56:48 +0100

    WE FOUND THE BLOODY MEMORY LEAK!!!!!!!!

--- service/src/main/java/eu/europa/ec/fisheries/uvms/movementrules/service/business/RulesValidator.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/movementrules/service/business/RulesValidator.java
@@ -30,6 +30,7 @@ import org.kie.api.KieServices;
 import org.kie.api.builder.KieFileSystem;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.FactHandle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import eu.europa.ec.fisheries.uvms.movementrules.model.dto.MovementDetails;
@@ -99,8 +100,10 @@ public class RulesValidator {
             ksession.setGlobal("validationService", validationService);
             ksession.setGlobal("logger", LOG);
 
-            ksession.insert(fact);
+            FactHandle factHandle = ksession.insert(fact);
             ksession.fireAllRules();
+
+            ksession.delete(factHandle);
         }
     }
 
